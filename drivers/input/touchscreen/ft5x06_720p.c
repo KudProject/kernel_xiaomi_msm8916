7c2ae83e1d6f5b1b82a0d45889769bcc2d79b248
Albert I <krascgq@outlook.co.id>
2017-02-20 18:55:57 +0800

    touchscreen: ft5x06: Use power efficient workqueue
    Change-Id: Iaade1f38b92fad118fb883fb298bcd4be3cd1e1e
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- drivers/input/touchscreen/ft5x06_720p.c
+++ drivers/input/touchscreen/ft5x06_720p.c
@@ -627,7 +627,8 @@ static int fb_notifier_callback(struct notifier_block *self,
 		blank = evdata->data;
 		if (*blank == FB_BLANK_UNBLANK || *blank == FB_BLANK_NORMAL
                  || *blank == FB_BLANK_VSYNC_SUSPEND || *blank == FB_BLANK_HSYNC_SUSPEND )
-		   schedule_work(&ft5x06_data->fb_notify_work);
+		   queue_work(system_power_efficient_wq,
+				&ft5x06_data->fb_notify_work);
 		 else if (*blank == FB_BLANK_POWERDOWN) {
 			flush_work(&ft5x06_data->fb_notify_work);
 			ft5x06_ts_suspend(&ft5x06_data->client->dev);
