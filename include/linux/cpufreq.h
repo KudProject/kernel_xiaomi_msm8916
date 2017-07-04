2a972b53841b5e513d35d9f9ec4bfc5f2dc11215
Albert I <krascgq@outlook.co.id>
2017-07-04 21:53:36 +0800

    cpufreq: Port cyan CPU governor from exynos5410 kernel platform
    Governor by emre3461 @ xda-developers.
    Ported to msm-3.10 kernel platorm over interactive since the governor
    it's based upon it. Primarily designed for high performance workloads.
    
    Modifications done:
    * Added support of two_phase_freq for hexa-core and octa-core
      processors.
    
    Current bug with this initial port:
    * -Wint-conversion warning with two_phase_freq
      I think it's there on the original version too if you build with GCC 5
      or newer. As workaround, the warning is disabled instead.
    * two_phase_freq shows 0,0,0,0,0,0,0,0; but can be overridden by your
      own values, so don't worry much.
    
    Original version is available at https://forum.xda-developers.com/android/software-hacking/governor-cyan-governor-android-t3299174
    
    Change-Id: Iafd77558e0be9ee4620afe8abfc49ee62e380639
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -442,6 +442,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NEBULA)
 extern struct cpufreq_governor cpufreq_gov_nebula;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_nebula)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CYAN)
+extern struct cpufreq_governor cpufreq_gov_cyan;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_cyan)
 #endif
 
 /*********************************************************************
